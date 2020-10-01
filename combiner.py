e33ac8b3743c77e8fd6a6a457ee783a9a558e787
Artem Strohin <artem.strohin@gmail.com>
2020-10-01 12:58:27 +0300

    fixed combiner + add new module

--- combiner.py
+++ combiner.py
@@ -5,24 +5,50 @@ import websockets
 import redis
 import concurrent.futures as treading
 
-# default_uri = 'ws://localhost:18081/ws'
+from time import time, sleep
+from unsync import unsync
+
+
 URI = os.environ.get('URI')
-
 r_conn = redis.Redis(host='redis', port=6379, db=0)
-period_list = [1, 5, 15, 60, 300, 900]
+PERIODS = [1, 5, 15, 60, 300, 900]
+CODES = []
 
 
-def save_to_redis(val, period=1):
-    key = get_key(val['code'], val['at'], period)
-    r_conn.xadd(key, val, id='*')
-    # TODO: need to set expire once when key is created
-    r_conn.expire(key, 16*60)
+def main():
+    tasks = [
+        redis_manager(),
+        data_flush()
+    ]
+
+    [t.result() for t in tasks]
 
 
-def get_from_redis(key):
-    # rv: list of tuples.
-    # Sample: [(b'1601465569624-0', {b'code': b'GBP/JPY', b'bid': b'43.77702805758552', b'ask': b'44.00702805758552', b'at': b'1601465569'})]
-    return r_conn.xrange(key, min='-', max='+')
+@unsync
+async def redis_manager():
+    async with websockets.connect(URI) as ws:
+        while True:
+            msg = await ws.recv()
+            data = json.loads(msg)
+
+            if data.get('code') and data['code'] not in CODES:
+                CODES.append(data['code'])
+
+            with treading.ThreadPoolExecutor() as executor:
+                executor.map(
+                    lambda x: save_to_redis(data, x),
+                    PERIODS
+                )
+
+
+@unsync
+def data_flush():
+    key_list = []
+    max_age = int(time()) - 16*60
+    while True:
+        for _code in CODES:
+            key_list += [get_key(_code, max_age, x) for x in PERIODS]
+        [r_conn.delete(_k) for _k in set(key_list)]
 
 
 def get_key(code, at, period):
@@ -30,19 +56,15 @@ def get_key(code, at, period):
     return f'at-{code}-{period}-{at_rounded}'
 
 
-# Sample: {'code': 'GBP/JPY', 'bid': 0.5584099998744797, 'ask': 0.7884099998744797, 'at': 1601384165}
-async def main(uri):
-    async with websockets.connect(uri) as ws:
-        while True:
-            msg = await ws.recv()
-            server_data = json.loads(msg)
-            # print(f'{server_data}')
-            with treading.ThreadPoolExecutor() as executor:
-                executor.map(
-                    lambda x: save_to_redis(server_data, x),
-                    period_list
-                )
+def save_to_redis(val, period):
+    key = get_key(val['code'], val['at'], period)
+    r_conn.xadd(key, val, id='*')
+
+
+def get_from_redis(key):
+    return r_conn.xrange(key, min='-', max='+')
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main(URI))
+    main()
+

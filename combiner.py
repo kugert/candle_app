9e687f335f4e5124d1274df633c1de9b3e887f5f
Artem Strohin <artem.strohin@gmail.com>
2020-10-05 21:37:35 +0300

    fixed Candle repository. Now awailable only 3 methods

--- combiner.py
+++ combiner.py
@@ -8,58 +8,51 @@ import concurrent.futures as treading
 from time import time, sleep
 from unsync import unsync
 
+URI = os.environ.get('URI')
+
 
 class CandleRepository:
-    def __init__(self, redis_params: dict = {'host': 'redis', 'port': 6379, 'db': 0}, periods: list = [1, 5, 15, 60, 300, 900], uri: str = ''):
-        self.redis_conn = redis.Redis(**redis_params)
-        self.periods = periods
-        self.uri = uri if uri else os.environ.get('URI')
-        self.codes = []
-
-    def __call__(self):
-        tasks = [
-            self.__redis_manager(),
-            self.__data_flush()
-        ]
-
-        [t.result() for t in tasks]
+    def __init__(self, redis_connection: redis.Redis, periods: list = [1, 5, 15, 60, 300, 900]):
+        self.__redis_conn = redis_connection
+        self.__periods = periods
+        self.__codes = []
 
     def get(self, from_at, period) -> list:
         rv = []
         key_list = [self.__get_key(_code, from_at, period)
-                    for _code in self.codes]
+                    for _code in self.__codes]
         for key in key_list:
             rv += self.__get_from_redis(key)
         return rv
 
-    def clear(self, before: int):
-        self.clear_before = int(time()) - before * 60
+    def clear_before(self, before: 'int (seconds)'):
+        self.__clear_before = int(time()) - before * 60
 
     @unsync
-    async def __redis_manager(self):
-        async with websockets.connect(self.uri) as ws:
+    async def store(self):
+        async with websockets.connect(URI) as ws:
             while True:
                 msg = await ws.recv()
                 data = json.loads(msg)
 
-                if data.get('code') and data['code'] not in self.codes:
-                    self.codes.append(data['code'])
+                if data.get('code') and data['code'] not in self.__codes:
+                    self.__codes.append(data['code'])
 
                 with treading.ThreadPoolExecutor() as executor:
                     executor.map(
                         lambda x: self.__save_to_redis(data, x),
-                        self.periods
+                        self.__periods
                     )
 
     @unsync
-    def __data_flush(self):
+    def clear(self):
         key_list = []
         while True:
-            for _code in self.codes:
-                key_list += [self.__get_key(_code, self.clear_before, x)
-                             for x in self.periods]
-            [self.redis_conn.delete(_k) for _k in set(key_list)]
-            sleep(0.75)
+            for _code in self.__codes:
+                key_list += [self.__get_key(_code, self.__clear_before, x)
+                             for x in self.__periods]
+            [self.__redis_conn.delete(_k) for _k in set(key_list)]
+            sleep(0.5)
 
     @staticmethod
     def __get_key(code, at, period):
@@ -68,9 +61,16 @@ class CandleRepository:
 
     def __save_to_redis(self, val, period):
         key = self.__get_key(val['code'], val['at'], period)
-        self.redis_conn.xadd(key, val, id='*')
+        self.__redis_conn.xadd(key, val, id='*')
 
     def __get_from_redis(self, key):
-        data = self.redis_conn.xrange(key, min='-', max='+')
-        return list(map(lambda x: x[1], data))
+        data = self.__redis_conn.xrange(key, min='-', max='+')
+        redis_list = list(map(lambda x: x[1], data))
+        return [self.__convert_byte_dict(x) for x in redis_list]
+
+    @staticmethod
+    def __convert_byte_dict(data: dict):
+        _keys = list(map(lambda x: x.decode('utf-8'), data.keys()))
+        _vals = list(map(lambda x: x.decode('utf-8'), data.values()))
+        return dict(zip(_keys, _vals))
 
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import time
+from redis import Redis
 from combiner import CandleRepository
+from unsync import unsync
+
+
+def main(r_conn):
+    candle = CandleRepository(r_conn)
+    tasks = [
+        candle.store(),
+        candle.clear()
+    ]
+
+    [t.result() for t in tasks]
 
 
 if __name__ == "__main__":
-    candle = CandleRepository()
-    candle()
-    time.sleep(5)
-    t = int(time.time()) - 2
-    print(candle.get(t, 1))
+    redis_conn = Redis(host='redis', port=6379, db=0)
+    main(redis_conn)
 
